7e13fb4b8ad800aef57593717e5a2746a92b8491
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-04 20:56:17 -0600

    prom

--- jav/promedio.java
+++ jav/promedio.java
@@ -5,5 +5,21 @@ public class ArregloEjemplo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Cantidad de numeros que desea ingresar...");
+        int n = scanner.nextInt();
+        int[] num = new int[n];
+        System.out.println("Ingrese esos numeros:");
+        for(int i=0;i<n;i++){
+            System.out.print("Numero "+(i + i)+ ":");
+            num[i] = scanner.nextInt();
+        }
+        int suma = 0;
+        for(int i=0;i<n:i++){
+            suma += num[i];
+        }
+        double promedio = (double)suma/n;
+        System.out.println("Suma = " + suma );
+        System.out.println("Promedio = " + promedio);
+        scanner.close();
     }
 }
