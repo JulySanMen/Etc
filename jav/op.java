c7f7e77b5cde5c7aefbb243fd30876c0c74fbb76
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-07 21:15:20 -0600

    opr

--- jav/op.java
+++ jav/op.java
@@ -2,9 +2,10 @@ public class op{
     public static void main(String[]args){
         int num1 = 8;
         int num2 = 3;
+        int rest = num1-num2;
 
         System.out.println(("8 + 3 = ")+ num1 + num2);
-        System.out.println(("8 - 3 = ")+ num1 - num2);
+        System.out.println(("8 - 3 = ")+ rest);
         System.out.println(("8 x 3 = ")+ num1 * num2);
         System.out.println(("8 / 3 = ")+ num1 / num2);
     }
