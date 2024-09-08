1a44a8498e7747e9cb2863c0f1c6211e7015e829
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-07 21:12:35 -0600

    op

---
+++ jav/op.java
@@ -0,0 +1,11 @@
+public class op{
+    public static void main(String[]args){
+        int num1 = 8;
+        int num2 = 3;
+
+        System.out.println(("8 + 3 = ")+ num1 + num2);
+        System.out.println(("8 - 3 = ")+ num1 - num2);
+        System.out.println(("8 x 3 = ")+ num1 * num2);
+        System.out.println(("8 / 3 = ")+ num1 / num2);
+    }
+}
