ecb4b2537977724e19a355311436b6df6e762a74
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-08 21:36:56 -0600

    ifi

---
+++ jav/ifi.java
@@ -0,0 +1,10 @@
+public class ifi{
+    public static void main(String[]args){
+        int nue = 1;
+        if(nue <= 50){
+            System.out.println("cincuenta");
+        }else{
+            System.out.println("uno");
+        }
+    }
+}
