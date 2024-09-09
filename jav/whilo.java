5f1142428cf6c0c523782a7df781083be5938ce9
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-08 21:23:27 -0600

    whilo

---
+++ jav/whilo.java
@@ -0,0 +1,9 @@
+public class whilo{
+    public static void main(String[]args){
+        int uno = 1;
+        while(uno<=100){
+            System.out.println(uno);
+            uno++;
+        }
+    }
+}
