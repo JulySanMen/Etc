2f64faabd40e16a171e6ad9981574cd37cda019d
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-09 20:58:17 -0600

    par

---
+++ jav/par.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class par{
+    public static void main(String[]args){
+        System.out.println("Ingrese un numero: ");
+        int num = scanner.nextInt();
+        if (num %2 ==0){
+            System.out.println("Su numero es par");
+        }else{
+            System.out.println("Su numero es inpar");
+        }
+    }
+    scanner.close();
+}
