c9b18cb27bf57f67c003e78e356e832b009c8ac6
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-22 22:11:30 -0600

    temcovercion

---
+++ jav/temp.java
@@ -0,0 +1,10 @@
+// Calcularemos temperatura 
+
+public class tem{
+    public static void main (String []args){
+        System.out.println("Ingrese temperatura a convertir: ")
+        int tem = 0;
+
+        
+    }
+}
