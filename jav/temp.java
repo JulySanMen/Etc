f39897ec3f4ee861df2077810a3856a06669d97c
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-22 22:27:22 -0600

    temp

--- jav/temp.java
+++ jav/temp.java
@@ -1,10 +1,26 @@
 // Calcularemos temperatura 
+import java.util.Scanner;
 
-public class tem{
+public class temp{
     public static void main (String []args){
-        System.out.println("Ingrese temperatura a convertir: ")
-        int tem = 0;
+        Scanner temp = new Scanner(System.in);
+        System.out.println("Ingrese temperatura en Celcius: ");
+        double Celcius = temp.nextDouble();
 
-        
+        double Kelvin = 273.15 + Celcius;
+
+        double Farenheit = 9/5. * Celcius + 32;
+
+        double Rankine = 9/5. *(Celcius + 273.15);
+
+        double Reaumur = 4/5. * Celcius;
+
+        System.out.println(Celcius + " grados Celcius valen: ");
+        System.out.println(Kelvin + " grados Kelvin");
+        System.out.println(Farenheit + " grados Farenheit");
+        System.out.println(Rankine + " grados Rankine");
+        System.out.println(Reaumur + " grados Reaumur");
+
+
     }
 }
