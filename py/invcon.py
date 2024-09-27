55920a394e063f673ac308d10957f72fa3b51195
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-26 20:50:12 -0600

    conver

--- py/invcon.py
+++ py/invcon.py
@@ -3,14 +3,13 @@ def conversioninv(binario):
     potencia = 0
 
     while binario > 0:
-        digito = binario % 10  # Obtener el último dígito del número binario
+        digito = binario % 10  
         decimal += digito * (2 ** potencia)
-        binario //= 10  # Eliminar el último dígito del número binario
+        binario //= 10  
         potencia += 1
 
     return decimal
 
-# Programa principal
 numero_binario = int(input("Introduce un número binario: "))
 numero_decimal = conversioninv(numero_binario)
 print(f"El número binario {numero_binario} en decimal es: {numero_decimal}")
