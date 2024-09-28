a153f0b2619bee90f936396ea843839064eb1faa
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-27 19:46:14 -0600

    fibo

---
+++ py/fobonachi.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    secuencia = []
+    a, b = 0, 1
+    for _ in range(n):
+        secuencia.append(a)
+        a, b = b, a + b
+    return secuencia
+
+n = int(input("Ingresa numero para ver su serie bibonacci : "))
+
+
+print(fibonacci(n))
