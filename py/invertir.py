5af09aa1635a4420a8f62ab6b776f0685c5ced65
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-09-28 23:27:41 -0600

    inversion

--- py/invertir.py
+++ py/invertir.py
@@ -1,9 +1,19 @@
-def invertir_cadena(cadena):
+#def invertir_cadena(cadena):
+ #   invertida = ""
+  #  for i in range(len(cadena) - 1, -1, -1):
+   #     invertida += cadena[i]
+    #return invertida
+
+#texto = "Hola mundo"
+#resultado = invertir_cadena(texto)
+#print(resultado)
+
+
+def invertir(cadena):
     invertida = ""
     for i in range(len(cadena) - 1, -1, -1):
         invertida += cadena[i]
     return invertida
-
-texto = "Hola mundo"
-resultado = invertir_cadena(texto)
-print(resultado)
+texto = input("Ingresa una frase o palabra: ")
+resultado = invertir(texto)
+print("Cadena invertida:", resultado)
