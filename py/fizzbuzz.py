15ff986f594038367e035c7d90784706b2cb1e1b
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-10-01 22:03:32 -0600

    fizbuz

--- py/fizzbuzz.py
+++ py/fizzbuzz.py
@@ -7,3 +7,7 @@ for i in range (1,100):
         print("buzz")
     else:
         print(i) 
+
+        #Se muestran los numero del 1 al 100 cuando haya multiplos de 3 y 5 se mostrara fizzbuzz
+        # Si solo hay multiplos de 3 se mostrara fizz 
+        # y si solo hay multiplos de 5 se mostrara buzz
