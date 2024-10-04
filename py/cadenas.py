a838789b799ee76961aa8ae627d28ab022f6ad21
JulySanMen <sanchezmendozajulieta@gmail.com>
2024-10-03 21:25:30 -0600

    cadenas

--- py/cadenas.py
+++ py/cadenas.py
@@ -1,9 +1,12 @@
-def diferencia_cadenas(str1, str2):
+def cadenas():
+    
+    str1 = input("Ingresa la primera cadena: ")
+    str2 = input("Ingresa la segunda cadena: ")
     
     out1 = ''.join([char for char in str1 if char not in str2])
-    
     out2 = ''.join([char for char in str2 if char not in str1])
     
-
     print("Out1:", out1)
     print("Out2:", out2)
+
+cadenas()
